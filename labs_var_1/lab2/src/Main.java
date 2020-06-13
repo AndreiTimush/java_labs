d8c6f723cd9a4acbb547206906dc40e9f2d43625
Andrey <andrikshumit@gmail.com>
2020-06-13 13:50:31 +0300

    Начало 2 лабораторной 1 варианта

--- labs_var_1/lab2/src/Main.java
+++ labs_var_1/lab2/src/Main.java
@@ -1,3 +1,46 @@
+class Creature{
+
+}
+class Snake extends Creature implements Wriggle{
+
+    @Override
+    public void creep() {
+
+    }
+
+    @Override
+    public void whoAmI() {
+
+    }
+
+    @Override
+    public void wriggle() {
+
+    }
+}
+
+class Dog extends Creature implements Creep{
+
+    @Override
+    public void creep() {
+
+    }
+
+    @Override
+    public void whoAmI() {
+
+    }
+}
+
+interface Creep{
+    void creep();
+    void whoAmI();
+}
+
+interface Wriggle extends Creep{
+    void wriggle();
+}
+
 public class Main {
     
 }
